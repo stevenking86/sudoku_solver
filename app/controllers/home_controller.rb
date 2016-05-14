f43e035fb3498dcf94246306d415610f346008fc
Joe Kim <joseph.joe.kim@gmail.com>
2016-05-14 11:21:21 -0400

    add timeout gem and timeout logic + reset button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,16 @@ class HomeController < ApplicationController
 
   def results
     sudoku_string = Solver.make_string(params)
-    @answer = Solver.solve(sudoku_string)
-    if request.xhr?
-      render :json => @answer
+    # if sudoku cannot complete, sudoku solver will timeout after 1 second
+    begin
+    	@result = Timeout::timeout(1) do
+		    @answer = Solver.solve(sudoku_string)
+	        if request.xhr?
+		    	render :json => @answer
+		    end
+    	end
+    rescue Timeout::Error
+    	render js: "alert('The Sudoku Board is invalid. Please reset and try again.');"
     end
   end
 
